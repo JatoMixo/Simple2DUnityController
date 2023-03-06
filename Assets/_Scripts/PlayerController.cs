a0140e8a8964887e077ff66dde65d67f88e6dbde
JatoMixo <jatomixo@outlook.es>
2023-03-06 16:16:17 +0100

    Started lateral movement

--- Assets/_Scripts/PlayerController.cs
+++ Assets/_Scripts/PlayerController.cs
@@ -2,15 +2,45 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
-{
-    void Start()
+namespace Player{
+    public class PlayerController : MonoBehaviour
     {
-        
-    }
+        [Header("X Movement")]
+        public float playerSpeed;
 
-    void Update()
-    {
+        [Space]
+        [Header("Jumping")]
+        public float jumpForce;
+        public float gravity;
         
+        [Space]
+        [Header("Components / Objects")]
+        [HideInInspector] public Rigidbody2D m_rigidbody;
+        [HideInInspector] public SpriteRenderer m_renderer;
+
+        void ConfigureComponents(){
+            m_rigidbody = gameObject.GetComponent<Rigidbody2D>();
+            m_rigidbody.gravityScale *= gravity;
+            m_renderer = gameObject.GetComponent<SpriteRenderer>();
+        }
+
+        void Start(){
+            ConfigureComponents();
+        }
+
+        void Update(){
+            LateralMovement();
+        }
+
+        void LateralMovement(){
+
+            int direction = (int)Input.GetAxisRaw("Horizontal");
+
+            SetRigidbodySpeed(playerSpeed * direction, m_rigidbody.velocity.y);
+        }
+
+        void SetRigidbodySpeed(float x, float y){
+            m_rigidbody.velocity = new Vector2(x * Time.deltaTime, y * Time.deltaTime);
+        }
     }
 }
